12c35210299a07da9e4d116fd17d49382feb628d
James Rhodes <jrhodes@redpointsoftware.com.au>
2011-07-30 04:52:42 +1000

    Added page frame allocation to mem.c

--- mem.c
+++ mem.c
@@ -1,67 +1,127 @@
 #include <system.h>
 #include <multiboot.h>
 
+/* Linked list structure for memory page allocate */
+struct mem_llist
+{
+	struct mem_llist* prev;
+	unsigned long long int start;
+	unsigned long long int length;
+	struct mem_llist* next;
+};
+
+/* Preallocated page to store the page frame allocation information */
+unsigned long long int page0 = 0;
+unsigned char page0alloc[1024] = { };
+struct mem_llist* pages = 0;
+
+/* Halts the system because there was a critical memory allocation
+ * failure within the kernel */
+void mem_fail()
+{
+	putch('\n');
+	settextcolor(4, 0);
+	puts("Kernel Out of Memory.  System Halted!\n");
+	for (;;);
+}
+
+/* Allocates raw memory within the 0th page and returns it */
+void* ralloc(unsigned int size)
+{
+	unsigned long long int pagestart = page0;
+	unsigned long long int chunkstart = 0;
+	unsigned long pagelen = 4096;
+	unsigned int i = 0;
+       	unsigned int s = 0;
+	while (i < pagelen / 4)
+	{
+		if (page0alloc[i] == 0 && s == 0)
+			chunkstart = pagestart + i * 4;
+		else if (page0alloc[i] == 0 && s < size / 4)
+			s++;
+		else if (page0alloc[i] == 0 && s == size / 4)
+		{
+			/* Found a chunk of appropriate size, so we
+			 * mark all of the appropriate indicies in
+			 * page0alloc as 1 */
+			for (i = 0; i < size / 4; i++)
+				page0alloc[(chunkstart - pagestart) / 4 + i] = 1;
+			return chunkstart;
+		}
+		else if (page0alloc[i] == 1)
+			s = 0;
+	}
+}
+
+/* Frees raw memory within the 0th page */
+void rfree(void* pos, unsigned int size)
+{
+	unsigned long long int pagestart = page0;
+	unsigned long long int chunkstart = pos;
+	unsigned int i = 0;
+	for (i = 0; i < size / 4; i++)
+		page0alloc[(chunkstart - pagestart) / 4 + i] = 1;
+}
+
 /* Registers the memory management system */
 void mem_install(struct multiboot_info* mbt, unsigned int magic)
 {
 	struct multiboot_memory_map* mmap = mbt->mmap_addr;
-	unsigned char itoabufferA[256];
-	unsigned char itoabufferB[256];
-	unsigned char itoabufferC[256];
-	unsigned char itoabufferD[256];
-//	long long addr = 0;
-//	long long length = 0;
+	unsigned long long int pagel = 0;
+	struct mem_llist* t = 0;
+	struct mem_llist* p = 0;
 
-	puts("Flags are: 0b");
-	itoa(mbt->flags, &itoabufferA, 2);
-	puts(itoabufferA);
-	puts(".\n");
+	/* Find the first available usable area above 1MB (so we
+	 * don't accidently run into kernel memory) */
+	while (mmap < mbt->mmap_addr + mbt->mmap_length && page0 == 0)
+	{
+		if (mmap->base_addr >= 1024 * 1024 /* addr >= 1MB */ && mmap->type == 1 /* is it usable memory? */)
+		{
+			/* Set page 0 to this address and then
+			 * exit the initial loop */
+			page0 = mmap->base_addr;
+			pagel = mmap->length;
+			break;
+		}
 
-	puts("Memory map starts at 0x");
-	itoa(mbt->mmap_addr, &itoabufferA, 16);
-	puts(itoabufferA);
-	puts(" and ends at 0x");
-	itoa(mbt->mmap_addr + mbt->mmap_length, &itoabufferA, 16);
-	puts(itoabufferA);
-	puts(" (length is 0x");
-	itoa(mbt->mmap_length, &itoabufferA, 16);
-	puts(itoabufferA);
-	puts(").\n");
+		mmap = (struct multiboot_memory_map*)((unsigned int)mmap + mmap->size + sizeof(unsigned int));
+	}
 
-	puts("Lower memory: 0x");
-	itoa(mbt->mem_lower, &itoabufferA, 16);
-	puts(itoabufferA);
-	puts(".\n");
-	puts("Upper memory: 0x");
-	itoa(mbt->mem_upper, &itoabufferA, 16);
-	puts(itoabufferA);
-	puts(".\n");
+	/* Check to see whether page0 is 0 (unset); if it is, we fail
+	 * because we don't have enough memory to store our page allocation
+	 * system */
+	if (page0 == 0)
+		mem_fail();
+	else
+		memset(page0alloc, 0, 1024);
 
+	/* Alright, so now we create the list by allocating the first element */
+	pages = ralloc(sizeof(struct mem_llist));
+	memset(pages, 0, sizeof(struct mem_llist));
+	pages->prev = 0;
+	pages->start = page0 + 4096;
+	pages->length = page0 + pagel;
+	pages->next = 0;
+
+	/* Now we mark the rest of our usable memory */
+	p = pages;
+	mmap = mbt->mmap_addr;
 	while (mmap < mbt->mmap_addr + mbt->mmap_length)
 	{
-		// Show what sections of memory are usable.
-		//addr = (mmap->base_addr_high << 32) | mmap->base_addr_low;
-		//length = (mmap->length_high << 32) | mmap->length_low;
+		if (mmap->base_addr != page0 || mmap->length != pagel)
+		{
+			/* This is a usable section of memory that was not
+			 * used as page0 */
+			t = ralloc(sizeof(struct mem_llist));
+			memset(t, 0, sizeof(struct mem_llist));
+			p->next = t;
+			t->prev = p;
+			t->start = mmap->base_addr;
+			t->length = mmap->length;
+			t->next = 0;
+			p = t;
+		}
 
-		itoa(mmap->base_addr, &itoabufferA, 16);
-		//itoa(mmap->base_addr_high, &itoabufferB, 16);
-		itoa(mmap->length, &itoabufferC, 16);
-		//itoa(mmap->length_high, &itoabufferD, 16);
-
-		puts(itoabufferA);
-		//puts(itoabufferB);
-		puts(" -> ");
-		puts(itoabufferC);
-		//puts(itoabufferD);
-		puts(": ");
-
-		if (mmap->type == 1)
-			puts("USABLE\n");
-		else
-			puts("UNUSABLE\n");
-
-		// ...
 		mmap = (struct multiboot_memory_map*)((unsigned int)mmap + mmap->size + sizeof(unsigned int));
 	}
 }
-
